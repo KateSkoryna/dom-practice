18f52a0d3ce46c467ec8dab2ba8587716f562245
Kateryna Skoryna <k.skoryna@gmail.com>
2022-07-20 11:01:43 +0200

    added json

--- event.js
+++ event.js
@@ -1,13 +1,36 @@
-const form = document.querySelector(".register-form");
+const form = document.querySelector('.register-form');
 
-form.addEventListener("submit", (event) => {
-    console.log("event: ", event);
-     console.log("event type: ", event.type);
-  console.log("currentTarget: ", event.currentTarget);
+form.addEventListener('submit', event => {
+  console.log('event: ', event);
+  console.log('event type: ', event.type);
+  console.log('currentTarget: ', event.currentTarget);
   event.preventDefault();
   const {
-    elements: { username, password }
+    elements: { username, password },
   } = event.currentTarget;
   console.log(username.value, password.value);
 });
 
+// const result = _.add(2, 3);
+// console.log(result);
+
+// document.addEventListener(
+//   'scroll',
+//   _.throttle(() => {
+//     console.log('Scroll handler call evenry 300ms');
+//   }, 300)
+// );
+
+document.addEventListener(
+  'scroll',
+  _.debounce(
+    () => {
+      console.log('Scroll handler call on every event stream start');
+    },
+    300,
+    {
+      leading: true,
+      trailing: false,
+    }
+  )
+);
